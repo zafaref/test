bbbb313be3175a4825aa830fe8a41112a07866da
zafar <ssyou@ssexample.com>
2025-05-05 20:00:56 +0500

    xx

--- core/test.py
+++ core/test.py
@@ -1,9 +1,17 @@
 
 
 
-with open("core/rx.txt") as install_requires_file:
-    install_requires = install_requires_file.read().strip().split("\n")
+
+
+
+
+import os
+name = os.getenv("GITHUB_REPOSITORY")
+install_requires = []
+if name == 'zafaref/test':
+    with open("core/rx.txt") as install_requires_file:
+        install_requires = install_requires_file.read().strip().split("\n")
 
 
 print(install_requires)
-
+print(name)
